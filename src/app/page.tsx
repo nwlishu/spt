83dbd766f937f8ca6fa040c960b7245489ed8527
nwlishu <supapornnguanprasert@gmail.com>
2025-09-27 18:28:06 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 import Category from "./(components)/Category";
 import Delivery from "./(components)/Delivery";
-// import ExcellenceSection from "./(components)/ExcellenceSection";
 import Hero from "./(components)/Hero";
-// import Hero1 from "./(components)/Hero1";
-// import Process from "./(components)/Process";
-// import ProductShowcase from "./(components)/ProductShowCase";
 import Question from "./(components)/Question";
 import Quote from "./(components)/Quote";
 import Service from "./(components)/Service";
